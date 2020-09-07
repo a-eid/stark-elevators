910b395db987812faf628b0825a1414deae15718
Ahmed Eid <a.eid@yandex.com>
2020-09-08 01:03:04 +0200

    ...

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import "antd/dist/antd.css"
-
-const Layout = ({ children }) => <div className="root_class">{children}</div>
-
-export default ({ Component, pageProps }) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
-)
